b653081ee2e354150a88ed4b29daac7f37252eeb
constantinus345 <96290754+constantinus345@users.noreply.github.com>
2023-08-27 11:39:54 +0300

    abc

--- extras/pubmed_ex.py
+++ extras/pubmed_ex.py
@@ -1,13 +1,13 @@
 import requests
-
 from bs4 import BeautifulSoup as bs
 
-link_1 = "https://pubmed.ncbi.nlm.nih.gov/29723297/"
+def get_doi_id(link_pubmed):
+    raspuns = requests.get(link_pubmed)
+    raspuns_soup = bs(raspuns.text, "html.parser")
+    clasa_doi = "identifier doi"
+    obiectul_doi = raspuns_soup.find("span", class_ = clasa_doi)
+    # print(obiectul_doi)
+    codul_doi = obiectul_doi.text.strip().replace("\n","").replace("DOI:", "").replace(" ","")
+    return codul_doi
 
-raspuns = requests.get(link_1)
-raspuns_soup = bs(raspuns.text, "html.parser")
-clasa_doi = "identifier doi"
-obiectul_doi = raspuns_soup.find("span", class_ = clasa_doi)
-# print(obiectul_doi)
-codul_doi = obiectul_doi.text.strip().replace("\n","").replace("DOI:", "").replace(" ","")
-print(codul_doi)
+print(get_doi_id("https://pubmed.ncbi.nlm.nih.gov/29723297/"))
